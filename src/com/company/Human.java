1ac95ce6b80f700055874b785fc19d31df280c35
CezaryCieslik <61739851+CezaryCieslik@users.noreply.github.com>
2020-05-04 19:30:58 +0200

    created classes

---
+++ src/com/company/Human.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class Human {
+    String firstName;
+    String lastName;
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Human me = new Human();
+	me.firstName = "Cezary";
+	me.lastName = "Cieślik";
+    }
+}
